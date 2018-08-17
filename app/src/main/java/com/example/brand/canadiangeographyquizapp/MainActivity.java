905f29692a0b6886a9cc94613dca8bd447b5b09e
Brandon Roberts <88.brandoncr@gmail.com>
2018-08-17 14:16:30 +0200

    updated

--- app/src/main/java/com/example/brand/canadiangeographyquizapp/MainActivity.java
+++ app/src/main/java/com/example/brand/canadiangeographyquizapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.brand.canadiangeographyquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -33,19 +34,51 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void totalScore() {
+    private int calculateScore() {
 
         radio_group_one = findViewById(R.id.radio_group_one);
-        int firstAnswer = radio_group_one.getCheckedRadioButtonId();
-        if (firstAnswer == -1)
+        int radioIdOne = radio_group_one.getCheckedRadioButtonId();
+        if (radioIdOne == -1)
+            return -1;
         correct_one = findViewById(R.id.right_1);
-        int firstAnswer1 = correct_one.getId();
-        if (firstAnswer1 == firstAnswer)
+        int radioButtonIdOne = correct_one.getId();
+        if (radioButtonIdOne == radioIdOne)
             quizScore++;
-        Toast toast = Toast.makeText(this, "correct answer", Toast.LENGTH_SHORT);
-        toast.show();
-    }
 
+        radio_group_two = findViewById(R.id.radio_group_two);
+        int radioIdTwo = radio_group_two.getCheckedRadioButtonId();
+        if (radioIdTwo== -1)
+            return -1;
+        correct_one = findViewById(R.id.right_2);
+        int radioButtonIdTwo = correct_two.getId();
+        if (radioButtonIdTwo == radioIdTwo)
+            quizScore++;
+
+        radio_group_three = findViewById(R.id.radio_group_three);
+        int radioIdThree = radio_group_three.getCheckedRadioButtonId();
+        if (radioIdThree== -1)
+            return -1;
+        correct_three = findViewById(R.id.right_3);
+        int radioButtonIdThree = correct_three.getId();
+        if (radioButtonIdThree == radioIdThree)
+            quizScore++;
+
+
+
+
+        return quizScore;
+
+
+    }
+public void submitScore() {
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Your Final Score is" + quizScore,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+}
     }
     
 
